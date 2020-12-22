42eb2341500d4cf09fdd0f8440b62f69de8f03cf
szubrilina <s.zubrilina2000@gmail.com>
2020-12-22 20:36:35 +0300

    remove built

--- built/CMakeFiles/SourceTesting_coverage.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SourceTesting_coverage"
-  "SourceTesting_coverage"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SourceTesting_coverage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
